f94a954dd93d05febb0dc7229f77d5aebd499c0b
markw53 <m.workman@outlook.com>
2025-03-06 19:37:07 +0000

    css changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-import App from "./App.jsx";
-import "./index.css";
+import App from "./App";
+import "./index.css";  // Ensure this is imported for global styles
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Router>
+            <App />
+        </Router>
+    </React.StrictMode>
 );
